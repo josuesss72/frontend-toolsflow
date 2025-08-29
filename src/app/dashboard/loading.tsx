57584985365b591af65b5ab735f119f3b6a44191
josuesss72 <jjdanielsarmiento@gmail.com>
2025-08-29 14:25:32 -0500

    feat: loading dashboard

---
+++ src/app/dashboard/loading.tsx
@@ -0,0 +1,9 @@
+function Loading() {
+	return (
+		<div className="sticky top-0 left-0 w-screen h-screen flex justify-center items-center bg-black">
+			<div className="w-6 h-6 border-4 border-t-transparent border-blue-500 rounded-full animate-spin" />
+		</div>
+	);
+}
+
+export default Loading;
