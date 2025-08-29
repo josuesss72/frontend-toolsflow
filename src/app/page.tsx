b5705f8b070e4df04cb401fda2a349a623d485f8
josuesss72 <jjdanielsarmiento@gmail.com>
2025-08-29 13:42:56 -0500

    feat: se cambio titulo de web, se agrego loading page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+"use client";
+import { useEffect } from "react";
+import LogoLoading from "./components/logo/LogoLoading";
+import { useRouter } from "next/navigation";
+
 export default function Home() {
-  return (
-    <div>
-      <h1 className="">Hola Mundo</h1>
-    </div>
-  );
+	const router = useRouter();
+	useEffect(() => {
+		setTimeout(() => {
+			router.push("/auth/login");
+		}, 5000);
+	}, []);
+	return (
+		<main>
+			<LogoLoading />
+		</main>
+	);
 }
